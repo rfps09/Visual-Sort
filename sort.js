c4b8be2294bfdc1d6da75f6aeb9cccec47c502bd
RAFAEL FELIPE <rafael09felipe@gmail.com>
2022-03-31 17:34:12 -0300

    problema ao chamar mais de uma função ao mesmo tempo

--- sort.js
+++ sort.js
@@ -5,19 +5,65 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function render(vetor, index) {
+function render(vetor, index = vetor.length, indexComparer = vetor.length, ordenado = false) {
     ctx.clearRect(0,0,cnv.width, cnv.height);
     var len = vetor.length;
     for(let i=0; i < len; i++) {
         if(i == index) ctx.fillStyle = "red";
+        else if(i == indexComparer) ctx.fillStyle = "green";
         else ctx.fillStyle = "#FFFFFF";
-        ctx.fillRect(80*i,600-60*vetor[i],80,60*vetor[i]);
+        if(ordenado == true) ctx.fillStyle = "#42ff33";
+        ctx.fillRect((800/len)*i,600-(600/len)*vetor[i],(800/len),(600/len)*vetor[i]);
     }
 }
 
-var vetorTeste = [9,7,3,8,6,2,5,1,4,10];
+var vetorTeste = [9,7,3,19,12,11,14,16,5,1,4,18,10,15,13,8,6,2,20,17];
+var base = vetorTeste.slice();
 
 render(vetorTeste);
+
+async function bubbleSort(vetor) {
+    var len = vetor.length;
+    for(let i=1; i < len; i++) {
+        for(let j=0; j < len-i; j++) {
+            render(vetor, j);
+            await sleep(250);
+            if(vetor[j] > vetor[j+1]) {
+                let aux = vetor[j];
+                vetor[j] = vetor[j+1];
+                vetor[j+1] = aux;
+            }
+            render(vetor, j+1);
+            await sleep(250);
+        }
+    }
+
+    render(vetor,vetor.length,vetor.length,true);
+    vetorTeste = base.slice();
+}
+
+async function selectionSort(vetor) {
+    var len = vetor.length;
+    var menor;
+    for(let i=0; i < len; i++) {
+        menor = i;
+        for(let j=i; j < len; j++) {
+            if(vetor[menor] > vetor[j]) {
+                menor = j;
+            }
+            render(vetor, j, menor);
+            await sleep(500);
+        }
+        let aux = vetor[i];
+        vetor[i] = vetor[menor];
+        vetor[menor] = aux;
+        render(vetor,vetor.length,i);
+        await sleep(500);
+    }
+
+    render(vetor,vetor.length,vetor.length,true);
+    vetorTeste = base.slice();
+}
 
 async function insertionSort(vetor) {
     var len = vetor.length;
@@ -35,26 +81,32 @@ async function insertionSort(vetor) {
         }
     }
 
-    render(vetor, len);
+    render(vetor,vetor.length,vetor.length,true);
+    vetorTeste = base.slice();
 }
 
-async function bubbleSort(vetor) {
+async function shellSort(vetor) {
     var len = vetor.length;
-    for(let i=2; i <= len; i++) {
-        for(let j=0; j < len-i; j++) {
-            render(vetor, j);
-            await sleep(250);
-            if(vetor[j] > vetor[j+1]) {
+    var gap = Math.floor(len/2);
+
+    while(gap) {
+        for(let i=gap; i < len; i++) {
+            let j=i;
+            render(vetor, i)
+            await sleep(500);
+            while(j-gap >= 0 && vetor[j] < vetor[j-gap]) {
                 let aux = vetor[j];
-                vetor[j] = vetor[j+1];
-                vetor[j+1] = aux;
+                vetor[j] = vetor[j-gap];
+                vetor[j-gap] = aux;
+                j-=gap;
+                render(vetor, j)
+                await sleep(500);
             }
-            render(vetor, j+1);
-            await sleep(250);
         }
+
+        gap = Math.floor(gap/2);
     }
 
-    render(vetor, len);
+    render(vetor,vetor.length,vetor.length,true);
+    vetorTeste = base.slice();
 }
-
-sleep(2000).then(()=>{insertionSort(vetorTeste);})
