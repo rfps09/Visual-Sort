df3092afd30103b14c972d55df25f6eb35201a21
RAFAEL FELIPE <rafael09felipe@gmail.com>
2022-04-10 17:08:03 -0300

    space between

--- js/sort.js
+++ js/sort.js
@@ -2,8 +2,8 @@ var cnv = document.getElementById("quadro");
 var ctx = cnv.getContext("2d");
 var ready = true;
 var para = false;
-var delay = 500;
-var tamanho = 20;
+var delay = 100;
+var tamanho = 50;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -13,6 +13,7 @@ function render(vetor, mapa = new Map()) {
     ctx.clearRect(0,0,cnv.width, cnv.height);
     var len = vetor.length;
     const estado = mapa.has("ordenado");
+    const space = 0.30;
 
     for(let i=0; i < len; i++) {
         ctx.fillStyle = "white";
@@ -20,12 +21,16 @@ function render(vetor, mapa = new Map()) {
         if(estado) ctx.fillStyle = mapa.get("ordenado");
         else if(mapa.has(i)) ctx.fillStyle = mapa.get(i);
         
-        ctx.fillRect((cnv.width/len)*i,cnv.height-(cnv.height/len)*vetor[i],(cnv.width/len),(cnv.height/len)*vetor[i]);
+        ctx.fillRect((cnv.width/len)*i-space,cnv.height-(cnv.height/len)*vetor[i],(cnv.width/len)-space,(cnv.height/len)*vetor[i]);
     }
 }
 
 var sequence = [10,8,4,9,7,3,6,2,5,1]
-var vetorTeste = [9,7,3,19,12,11,14,16,5,1,4,18,10,15,13,8,6,2,20,17];
+var vetorTeste = 
+[25,50,14,6,48,19,1,8,4,20,18,33,30,
+5,29,32,24,3,36,37,41,39,17,31,16,49,
+21,35,40,43,2,13,47,7,46,22,44,38,45,
+34,23,42,28,15,9,11,27,26,10,12];
 var base = vetorTeste.slice();
 
 render(vetorTeste);
