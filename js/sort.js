4cc7e9a96516b45b2b9aa5c491a5de6536552a25
rfps09 <rafael09felipe@gmail.com>
2022-06-11 15:40:39 -0300

    arrumando detalhes

--- js/sort.js
+++ js/sort.js
@@ -4,6 +4,16 @@ var ready = true;
 var para = false;
 var delay = 100;
 var tamanho = 50;
+var seqIndex = 5;
+var seqDelay = [1,5,10,25,50,100];
+
+var sequence = [10,8,4,9,7,3,6,2,5,1];
+var vetorTeste = 
+[25,50,14,6,48,19,1,8,4,20,18,33,30,
+5,29,32,24,3,36,37,41,39,17,31,16,49,
+21,35,40,43,2,13,47,7,46,22,44,38,45,
+34,23,42,28,15,9,11,27,26,10,12];
+var base = vetorTeste.slice();
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -13,27 +23,18 @@ function render(vetor, mapa = new Map()) {
     ctx.clearRect(0,0,cnv.width, cnv.height);
     var len = vetor.length;
     const estado = mapa.has("ordenado");
-    const space = 0.30;
+    let space = 0;
 
     for(let i=0; i < len; i++) {
+        if(i>0) space = cnv.width/len*0.03; 
         ctx.fillStyle = "white";
 
         if(estado) ctx.fillStyle = mapa.get("ordenado");
         else if(mapa.has(i)) ctx.fillStyle = mapa.get(i);
         
-        ctx.fillRect((cnv.width/len)*i-space,cnv.height-(cnv.height/len)*vetor[i],(cnv.width/len)-space,(cnv.height/len)*vetor[i]);
+        ctx.fillRect((cnv.width/len)*i+space,cnv.height-(cnv.height/len)*vetor[i],(cnv.width/len)-space,(cnv.height/len)*vetor[i]);
     }
 }
-
-var sequence = [10,8,4,9,7,3,6,2,5,1]
-var vetorTeste = 
-[25,50,14,6,48,19,1,8,4,20,18,33,30,
-5,29,32,24,3,36,37,41,39,17,31,16,49,
-21,35,40,43,2,13,47,7,46,22,44,38,45,
-34,23,42,28,15,9,11,27,26,10,12];
-var base = vetorTeste.slice();
-
-render(vetorTeste);
 
 function ordenacao(ordenar){
     if(ready){
@@ -60,24 +61,36 @@ function reset(estado = "desordenado") {
 
 function Delay(acao) {
     if(delay > 100 || (acao=='+' && delay==100)) {
-        if(acao == '+' && delay < 1000) delay += 50;
-        else if(acao == '-' && delay >= 100) delay -= 50;
+        if(acao == '+' && delay < 1000) delay += 100;
+        else if(acao == '-' && delay > 100) delay -= 100;
     }
     else {
-        if(acao == '+' && delay < 100) delay += 10;
-        else if(acao == '-' && delay >= 20) delay -= 10;
+        if(acao == '+' && delay < 100) {
+            seqIndex++;
+            delay = seqDelay[seqIndex];
+        }
+        else if(acao == '-' && delay >= 5) {
+            seqIndex--;
+            delay = seqDelay[seqIndex];
+        }
     }
     document.getElementById('velocidade').innerHTML = delay;
 }
 
 function Tamanho(acao) {
     if(ready) {
-        let TamAtual = tamanho;
-        if(acao == '+' && tamanho < 200) tamanho += 10;
-        else if(acao == '-' && tamanho >= 20) tamanho -= 10;
+        const Tam = tamanho;
+        if(tamanho > 100 || (acao=='+' && tamanho==100)) {
+            if(acao == '+' && tamanho < 500) tamanho += 100;
+            else if(acao == '-' && tamanho > 100) tamanho -= 100;
+        }
+        else {
+            if(acao == '+' && tamanho < 100) tamanho += 10;
+            else if(acao == '-' && tamanho > 10) tamanho -= 10;
+        }
         document.getElementById('TamanhoVetor').innerHTML = tamanho;
 
-        if(TamAtual != tamanho) fillAndDisorderVector(tamanho);
+        if(Tam != tamanho) fillAndDisorderVector(tamanho);
     }
 }
 
@@ -100,3 +113,5 @@ function fillAndDisorderVector(tam) {
     vetorTeste = base.slice();
     render(vetorTeste);
 }
+
+render(vetorTeste);
